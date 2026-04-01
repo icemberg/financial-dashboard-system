4b6192bba00e8905254bc207ec72855351da3336
icemberg <abhiraams2004@gmail.com>
2026-04-01 18:01:44 +0530

    first commit

---
+++ src/test/java/com/financedashboard/zorvyn/ZorvynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financedashboard.zorvyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZorvynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
